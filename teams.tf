81197a2d63a3a704126903786b6d7cb49be2d9ff
Manuel Florez <manuelflorezw@gmail.com>
2022-07-15 07:36:34 -0500

    add teams.tf

---
+++ teams.tf
@@ -0,0 +1,21 @@
+resource "github_team" "developer_team" {
+  name        = "developer"
+  description = "Team developer"
+}
+
+resource "github_team" "approvers_team" {
+  name        = "approvers"
+  description = "Team approvers"
+}
+
+resource "github_team_repository" "team_repository_developer" {
+  team_id    = github_team.developer_team.id
+  repository = github_repository.repository.name
+  permission = "pull"
+}
+
+resource "github_team_repository" "team_repository_approvers" {
+  team_id    = github_team.approvers_team.id
+  repository = github_repository.repository.name
+  permission = "pull"
+}
