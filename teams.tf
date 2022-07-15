0316c53bff50c51ca21f7f13b7649149262c3f0d
Manuel Florez <manuelflorezw@gmail.com>
2022-07-15 08:20:00 -0500

    add github_team_members

--- teams.tf
+++ teams.tf
@@ -23,7 +23,7 @@ resource "github_team_repository" "team_repository_approvers" {
 resource "github_team_members" "developer_team_members" {
   team_id  = github_team.developer_team.id
 
-  member {
+  members {
     for_each = toset(var.developers)
     username = each.key
     role     = "member"
